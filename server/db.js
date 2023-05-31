d9856b5dd92c465c64f0077f7a8ac9a3ed9d1add
EnzoCam7 <camerac09@gmail.com>
2023-05-31 20:21:22 -0300

    Connection to DB

---
+++ server/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+
+export const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_DB)
+        console.log(`Mongo DB connected successfully : ${conn.connection.name}` )
+    } catch (error) {
+        console.error(`Error: ${error.message}`)
+        process.exit(1)
+    }
+}
--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 import { startApollo } from "./app.js"
 import { typeDefs } from "./graphql/typeDef.js"
 import { resolvers } from "./graphql/resolvers.js"
+import dotenv from "dotenv"
+import { connectDB } from "./db.js"
 
+
+dotenv.config()
+connectDB()
 startApollo(typeDefs, resolvers)
