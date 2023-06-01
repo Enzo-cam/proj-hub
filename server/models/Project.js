323d7cd811930805c28a05da4914052d3cfd5b88
EnzoCam7 <camerac09@gmail.com>
2023-06-01 18:21:27 -0300

    Project schema

--- server/models/Project.js
+++ server/models/Project.js
@@ -1,15 +1,18 @@
 import mongoose from "mongoose";
 
-const ProjectSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required: true
+const ProjectSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    description:{
-        type: String,
+    description: {
+      type: String,
     },
-},{
+  },
+  {
     timestamps: true,
-})
+  }
+);
 
-export default mongoose.model('Project', ProjectSchema)
+export default mongoose.model("Project", ProjectSchema);
