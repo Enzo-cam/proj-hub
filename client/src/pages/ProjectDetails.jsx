2d7d823f519708455d8517410d752f89a395f74c
EnzoCam7 <camerac09@gmail.com>
2023-06-04 20:19:54 -0300

    Details about projects

--- client/src/pages/ProjectDetails.jsx
+++ client/src/pages/ProjectDetails.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { GET_PROJECT } from "../graphql/projects";
+import TasksList from '../components/Task/TasksList'
+import TaskForm from "../components/Task/TaskForm";
 
 const ProjectDetails = () => {
-  return (
-    <div>ProjectDetails</div>
-  )
-}
+  const params = useParams();
+  const { loading, error, data } = useQuery(GET_PROJECT, {
+    variables: {
+      id: params.id,
+    },
+    skip: !params.id,
+  });
 
-export default ProjectDetails
+  if (loading) return <p>The project is loading</p>;
+
+  return (
+    <div>
+      <h1>{data.project.name}</h1>
+      <p>{data.project.description}</p>
+      
+      <button>Delete</button>
+      <TaskForm/>
+      <TasksList tasks={data.project.tasks} />
+    </div>
+  );
+};
+
+export default ProjectDetails;
