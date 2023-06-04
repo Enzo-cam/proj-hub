2ca75ea8a8871f830a9303572aa90757315ecb35
EnzoCam7 <camerac09@gmail.com>
2023-06-04 16:49:03 -0300

    Setup Frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Projects from "./pages/Projects";
+import ProjectDetails from "./pages/ProjectDetails";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5050/graphql",
+  // Lo guarda en cache, no solicita los datos si ya estan en memoria.
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/projects"/>} />
+          <Route path="/projects" element={<Projects/>} />
+          <Route path="/projects/:id" element={<ProjectDetails/>} />
+        </Routes>
+      </BrowserRouter>
+    </ApolloProvider>
+  );
 }
 
-export default App
+export default App;
