da6f424be0cbfbf61d9dccffd72f2659f3ef2320
EnzoCam7 <camerac09@gmail.com>
2023-06-04 16:48:40 -0300

    Query to Backend

---
+++ client/src/graphql/projects.js
@@ -0,0 +1,14 @@
+import {gql} from '@apollo/client'
+
+export const GET_PROJECTS = gql`
+    {
+        projects{
+            _id
+            name
+            description
+            tasks{
+                title
+            }
+        }
+    }
+`
