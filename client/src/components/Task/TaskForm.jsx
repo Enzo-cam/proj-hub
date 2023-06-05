5f21a434c54056855360824a15995c07705acc3d
EnzoCam7 <camerac09@gmail.com>
2023-06-05 18:50:56 -0300

    Task form to create tasks

--- client/src/components/Task/TaskForm.jsx
+++ client/src/components/Task/TaskForm.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import {useMutation} from '@apollo/client'
+import { CREATE_TASK } from "../../graphql/task"
+import {useParams} from "react-router-dom"
 
 const TaskForm = () => {
+  const params = useParams()
+  const [createTask] = useMutation(CREATE_TASK, {
+    // Una vez ejecutado el Create, vamos a hacer un llamado a Get para actualizar la lista de los proyectos
+    refetchQueries: ["getProject"]
+  })
+
+  const handleSubmit = async e =>{
+    e.preventDefault()
+    await createTask({
+      variables: {
+        title: e.target.title.value,
+        projectId: params.id
+      }
+    })
+    e.target.reset()
+    e.target.title.focus()
+  }
+
   return (
-    <div>TaskForm</div>
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="title" />
+      <button>Añadir tarea</button>
+    </form>
   )
 }
 
